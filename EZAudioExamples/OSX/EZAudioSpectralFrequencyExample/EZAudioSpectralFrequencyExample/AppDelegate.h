754552424dedd45154c4103aba5d5dec9f3a5bd2
Yuan Zhaokai <netiger@Netiger.local>
2015-10-14 16:42:41 +0800

    Initial Setup

---
+++ EZAudioExamples/OSX/EZAudioSpectralFrequencyExample/EZAudioSpectralFrequencyExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EZAudioSpectralFrequencyExample
+//
+//  Created by Netiger on 10/12/15.
+//  Copyright © 2015 Sadcup. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ EZAudioExamples/OSX/EZAudioSpectralFrequencyExample/EZAudioSpectralFrequencyExample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  EZAudioSpectralFrequencyExample
+//
+//  Created by Netiger on 10/12/15.
+//  Copyright © 2015 Sadcup. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
